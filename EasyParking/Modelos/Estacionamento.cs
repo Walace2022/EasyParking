763a63d8ebeaae48b243f5dbdd17e450712c8030
Henrique Walace <henriquewalace@hotmail.com>
2024-05-13 17:25:33 -0300

    feat: Criar Classe Estacionamento

---
+++ EasyParking/Modelos/Estacionamento.cs
@@ -0,0 +1,28 @@
+﻿
+namespace EasyParking.Modelos;
+
+internal class Estacionamento
+{
+    public decimal precoInicial { get; set; }
+    public decimal precoPorHora { get; set; }
+    private List<string> Veiculos = new List<string>();
+    
+    public void AdicionarVeiculos(string veiculo)
+    {
+        Veiculos.Add(veiculo);
+    }
+
+    public void RemoverVeiculos(string veiculo)
+    {
+        Veiculos.Remove(veiculo);
+    }
+
+    public void ListarVeiculos()
+    {
+        Console.WriteLine("Veiculos Cadastrados no Estacionamento");
+        foreach (string veiculo in Veiculos)
+        {
+            Console.WriteLine($" - {veiculo}");
+        }
+    }
+}
