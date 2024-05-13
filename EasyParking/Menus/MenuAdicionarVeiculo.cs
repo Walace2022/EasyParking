4fc00a9eef95841e8fd85ae820fbe08405b4a7b5
Henrique Walace <henriquewalace@hotmail.com>
2024-05-13 17:42:56 -0300

    feat: Cria Classe MenuAdicionarVeiculo

---
+++ EasyParking/Menus/MenuAdicionarVeiculo.cs
@@ -0,0 +1,16 @@
+﻿using EasyParking.Modelos;
+
+namespace EasyParking.Menus;
+
+internal class MenuAdicionarVeiculo : Menu
+{
+    public override void Executar(Estacionamento estacionamento)
+    {
+        base.Executar(estacionamento);
+        ExibirTituloDoMenu("Adicione um Veiculo");
+        Console.Write("Digite a placa do veiculo a ser adicionado: ");
+        string placa = Console.ReadLine()!;
+        estacionamento.AdicionarVeiculos(placa);
+        voltarParaMenuPrincipal();
+    }
+}
