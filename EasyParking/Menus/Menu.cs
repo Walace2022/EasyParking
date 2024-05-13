f47ff6b5f3b7edae1edcfe820f52513d26eb5461
Henrique Walace <henriquewalace@hotmail.com>
2024-05-13 17:34:52 -0300

    feat: Criar Classe Menu

---
+++ EasyParking/Menus/Menu.cs
@@ -0,0 +1,20 @@
+﻿using EasyParking.Modelos;
+
+namespace EasyParking.Menus;
+
+internal class Menu
+{
+    public void ExibirTituloDoMenu(string titulo)
+    {
+        int tamanhoDoTitulo = titulo.Length;
+        string detalhe = string.Empty.PadLeft(tamanhoDoTitulo, '=');
+        Console.WriteLine(detalhe);
+        Console.WriteLine(titulo);
+        Console.WriteLine(detalhe + "\n");
+    }
+
+    public virtual void Executar(Estacionamento estacionamento)
+    {
+        Console.Clear();
+    }
+}
