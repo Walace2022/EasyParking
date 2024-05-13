ac280dd7f84b6a9ef89bbf5e59fe6e7dd7eb23ed
Henrique Walace <henriquewalace@hotmail.com>
2024-05-13 18:06:09 -0300

    feat: Cria Classe MenuEncerrar

---
+++ EasyParking/Menus/MenuEncerrar.cs
@@ -0,0 +1,13 @@
+﻿
+using EasyParking.Modelos;
+
+namespace EasyParking.Menus;
+
+internal class MenuEncerrar : Menu
+{
+    public override void Executar(Estacionamento estacionamento)
+    {
+        base.Executar(estacionamento);
+        Console.WriteLine("Obrigado por usar o EasyParking.");
+    }
+}
